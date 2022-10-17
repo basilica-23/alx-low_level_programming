b2b05fa76c9a91d71d38575c6accf7009b3d4b18
basilica-23 <basilica.benji@gmail.com>
2022-10-17 10:56:52 -0700

    error change

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 =  0; num1 < 10; num1++)
+	for (num1 =  0; num1 < 9; num1++)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			putchar((num1 % 10) + '0');
+			putchar((num1 % 9) + '0');
 			putchar((num2 % 10) + '0');
 
 			if (num1 == 9 && num2 == 9)
