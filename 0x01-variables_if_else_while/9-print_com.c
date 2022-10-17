67cf44932ee0a7716a3ec0df6b1d8d0b37f26eb8
basilica-23 <basilica.benji@gmail.com>
2022-10-17 04:54:41 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_com.c
+++ 0x01-variables_if_else_while/9-print_com.c
@@ -2,20 +2,22 @@
 /**
  * main - prints all possible combinations of single-digit num
  *
- * Return: (0)
- *
+ * Return:(0)
  */
 int main(void)
 {
-	char n;
+	int n;
 
 	for (n = '0'; n <= '9'; n++)
-		putchar(n);
-	if (n != '9')
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(n);
+
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
+	put('\n');
 	return (0);
 }
