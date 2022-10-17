2d09ce06833198da5edf483911379e9434978edc
basilica-23 <basilica.benji@gmail.com>
2022-10-17 01:48:38 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints the lowercase alphabet in reverse
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char revLa;
+
+	for (revLa = 'z'; revLa < 'a'; revLa--)
+		putchar(revLa);
+	putchar('\n');
+	return (0);
+}
