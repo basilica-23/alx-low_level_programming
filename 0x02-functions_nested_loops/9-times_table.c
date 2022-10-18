58a13345ed29518e26bdfc6422a56dd56c70e1a1
basilica-23 <basilica.benji@gmail.com>
2022-10-18 15:29:49 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - a function that prints the 9 times tables, starting with 0
+ * r = row, cl = column, d = digits of current result
+ * Return: times table
+ * add extre space past single digit
+ */
+void times_table(void)
+{
+	int r, cl, d;
+
+	for (r = 0; r <= 9; cl++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cl = 1; cl <= 9; cl++)
+		{
+			d = (r * cl);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+			if (cl < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
