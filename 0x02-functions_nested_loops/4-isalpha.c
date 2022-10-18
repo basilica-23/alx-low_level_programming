18646fb0855c451e90e6ac0ee50b683d75f12f50
basilica-23 <basilica.benji@gmail.com>
2022-10-18 14:34:31 -0700

    alphabetuc check

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabets
- * @c: character to be checked
- *
- * Return 1 if checker isalpha,0 otherwise
+ * _isalpha - a function that checks for alphabetic character
+ * @c: single letter input
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
