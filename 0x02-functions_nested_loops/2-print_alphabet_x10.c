a9e28ebbbbc3682e39c58ec8d1e333cf786e538a
basilica-23 <basilica.benji@gmail.com>
2022-10-18 13:10:20 -0700

    print alphabet x ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ntine;
+	char l;
+
+	for (nten = 0; nten <= 9; nten++)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+			_putchar(l);
+		_putchar('\n');
+	}
+}
