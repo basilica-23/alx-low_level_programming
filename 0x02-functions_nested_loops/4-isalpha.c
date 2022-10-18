2051cc62e766a6e03fd645f4869ff4a8947aa672
basilica-23 <basilica.benji@gmail.com>
2022-10-18 13:50:35 -0700

    alphachanges

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * _isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return 1 if checker is a letter, 0 otherwise
+ * Return 1 if checker isalpha,0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' $$ c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
