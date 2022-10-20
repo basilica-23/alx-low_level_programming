45bdb8350268afc04c7093f64d081747753aa9e2
basilica-23 <basilica.benji@gmail.com>
2022-10-20 05:00:27 -0700

    all fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,5 @@
 void positive_or_negative(int i);
 int convent_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 #endif
