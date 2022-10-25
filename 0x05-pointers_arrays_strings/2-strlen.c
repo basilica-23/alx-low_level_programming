6b2fdcd44914e4230af2699b0e6133bba1f5c905
benjamin-23 <kitongabenja34@gmail.com>
2022-10-25 13:40:01 +0300

    fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * _strlen - returns the length of a string
  * @s: input string
  *
  * Return: length of s
  */
-int _strlen(int *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
